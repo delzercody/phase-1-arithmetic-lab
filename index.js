c3bddea74b13432b39daeb10eb265006e243bcd0
Cody Delzer <delzercody@gmail.com>
2023-04-11 23:40:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const min = 1
+const max = 20
+let random = Math.floor(Math.random() * (max - min) + min)
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4
+
+
+console.log(random)
+/*function multiply() {
+    num1 * num2
+}*/
+
+
+
+
+//past attempt that didn't work
+/*function multiply(num1 = Number, num2 = Number) {
+   // let num1 = Number;
+   // let num2 = Number;
+    //num1 = Number;
+    //let answer = num1 * num2;
+   return num1 * num2;
+}*/
+// tried to write as a function but may be overcomplicating it. 
+// need to look closer at the test.
+
+//multiply(2, 31)
+
+//console.log (2 * 31)
